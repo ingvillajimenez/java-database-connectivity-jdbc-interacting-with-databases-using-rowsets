40842cc055bea4469e0e375d16d16d2615c8f15e
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-19 11:02:58 -0600

    Updating Table Data with a CachedRowSet

--- jdbc-app/src/main/java/com/loonycorn/ExploringRowSets.java
+++ jdbc-app/src/main/java/com/loonycorn/ExploringRowSets.java
@@ -1,7 +1,8 @@
 package com.loonycorn;
 
-import javax.sql.RowSet; // interface
+import javax.sql.RowSet; // interface RowSet
 import javax.sql.rowset.CachedRowSet; // interface CachedRowSet
+import java.sql.Connection; // interface Connection
 import java.sql.SQLException; // class SQLException
 
 public class ExploringRowSets {
@@ -19,79 +20,123 @@ public class ExploringRowSets {
 
     public static void main(String[] args) {
 
-        try (CachedRowSet cachedRS = DBUtils.getCachedRowSet("DeliveryService")) {
+        try (Connection conn = DBUtils.getMysqlConnection("DeliveryService")) {
+
+            conn.setAutoCommit(false);
+
+            CachedRowSet cachedRS = DBUtils.getCachedRowSet("");
 
             cachedRS.setCommand("select * from delpartners");
-            cachedRS.execute();
+            cachedRS.execute(conn);
 
-            System.out.println("Moving around in a CachedRowSet: \n");
+            int updatedRows = 0;
 
-            cachedRS.first();
-            displayRow("first()", cachedRS);
-            //first(): Adam | Bell | 18.50 | true
+            while (cachedRS.next()) {
 
-            cachedRS.relative(2);
-            displayRow("relative(2)", cachedRS);
-            //relative(2): Pam | Cruz | 21.00 | true
+                if (!cachedRS.getBoolean("is_fulltime")) {
 
-            cachedRS.previous();
-            displayRow("previous()", cachedRS);
-            //previous(): Eric | Jones | 23.00 | false
+                    cachedRS.updateDouble("hourly_rate", 26.0);
+                    cachedRS.updateRow();
 
-            cachedRS.absolute(4);
-            displayRow("absolute(4)", cachedRS);
-            //absolute(4): Gav | Comey | 19.00 | true
+                    displayRow("Updated record: ", cachedRS);
+                    //Updated record: : Eric | Jones | 26.00 | false
+                    //
+                    //Updated record: : Stacey | Shields | 26.00 | false
+                    //
+                    //Updated record: : Kylie | Kass | 26.00 | false
+                    updatedRows++;
+                }
+            }
 
-            cachedRS.last();
-            displayRow("last()", cachedRS);
-            //last(): Kylie | Kass | 25.00 | false
-            // a new connection was established so that the update in the DB was read
+            cachedRS.acceptChanges(conn);
 
-            cachedRS.relative(-1);
-            displayRow("relative(-1)", cachedRS);
-            //relative(-1): Pablo | Hernandez | 19.50 | true
+            cachedRS.close();
+
+            System.out.println("\nNumber of updated rows: " + updatedRows);
+            //Number of updated rows: 3
+            // the update has taken effect in the DB
+
         }
         catch (SQLException ex) {
             ex.printStackTrace();
         }
+
+/*        try (CachedRowSet cachedRS = DBUtils.getCachedRowSet("DeliveryService")) {
+
+            cachedRS.setCommand("select * from delpartners");
+            cachedRS.execute();
+
+//            cachedRS.setAutoCommit(true); // Cannot resolve method 'setAutoCommit' in 'CachedRowSet'
+
+            int updatedRows = 0;
+
+            while (cachedRS.next()) {
+
+                if (!cachedRS.getBoolean("is_fulltime")) {
+
+                    cachedRS.updateDouble("hourly_rate", 25.0);
+                    cachedRS.updateRow();
+
+                    displayRow("Updated record: ", cachedRS);
+                    //Updated record: : Eric | Jones | 25.00 | false
+                    //
+                    //Updated record: : Stacey | Shields | 25.00 | false
+                    //
+                    //Updated record: : Kylie | Kass | 25.00 | false
+                    //In spite of the exception, the update did in fact get pushed through to the DB
+                    updatedRows++;
+                }
+            }
+
+            cachedRS.acceptChanges();
+            //java.sql.SQLException: Can't call commit when autocommit=true
+            //	at com.mysql.cj.jdbc.exceptions.SQLError.createSQLException(SQLError.java:67)
+            //	at com.mysql.cj.jdbc.ConnectionImpl.commit(ConnectionImpl.java:782)
+            //	at java.sql.rowset/com.sun.rowset.internal.CachedRowSetWriter.commit(CachedRowSetWriter.java:1408)
+            //	at java.sql.rowset/com.sun.rowset.CachedRowSetImpl.acceptChanges(CachedRowSetImpl.java:904)
+            //	at com.loonycorn.ExploringRowSets.main(ExploringRowSets.java:48)
+            //javax.sql.rowset.spi.SyncProviderException: Can't call commit when autocommit=true
+            //	at java.sql.rowset/com.sun.rowset.CachedRowSetImpl.acceptChanges(CachedRowSetImpl.java:923)
+            //	at com.loonycorn.ExploringRowSets.main(ExploringRowSets.java:48)
+
+            System.out.println("\nNumber of updated rows: " + updatedRows);
+
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }*/
 
 //        try (CachedRowSet cachedRS = DBUtils.getCachedRowSet("DeliveryService")) {
 //
 //            cachedRS.setCommand("select * from delpartners");
 //            cachedRS.execute();
 //
-//            System.out.println("Moving around in a CachedRowSet: \n");
+//            int updatedRows = 0;
 //
-//            cachedRS.first();
-//            displayRow("first()", cachedRS);
-//            //first(): Adam | Bell | 18.50 | true
+//            while (cachedRS.next()) {
 //
-//            cachedRS.relative(2);
-//            displayRow("relative(2)", cachedRS);
-//            //relative(2): Pam | Cruz | 21.00 | true
+//                if (!cachedRS.getBoolean("is_fulltime")) {
 //
-//            cachedRS.previous();
-//            displayRow("previous()", cachedRS);
-//            //previous(): Eric | Jones | 23.00 | false
+//                    cachedRS.updateDouble("hourly_rate", 25.0);
+//                    cachedRS.updateRow();
 //
-//            cachedRS.absolute(4);
-//            displayRow("absolute(4)", cachedRS);
-//            //absolute(4): Gav | Comey | 19.00 | true
+//                    displayRow("Updated record: ", cachedRS);
+//                    //Updated record: : Eric | Jones | 25.00 | false
+//                    //
+//                    //Updated record: : Stacey | Shields | 25.00 | false
+//                    //
+//                    //Updated record: : Kylie | Kass | 25.00 | false
+//                    updatedRows++;
+//                }
+//            }
 //
-//            System.out.println("\nSleeping for a minute...");
-//            Thread.sleep(60000);
+//            System.out.println("\nNumber of updated rows: " + updatedRows);
+//            //Number of updated rows: 3
+//            //The update has not taken place at the database end because there was not a commit.
+//            // It took place at the cachedRS
 //
-//            cachedRS.last();
-//            cachedRS.refreshRow();
-//            displayRow("last()", cachedRS);
-//            //last(): Kylie | Kass | 22.00 | false
-//            //cachedRS was not able to pick up the updated value in the DB because cachedRS is disconnected to the DB
-//
-//            cachedRS.relative(-1);
-//            displayRow("relative(-1)", cachedRS);
-//            //relative(-1): Pablo | Hernandez | 19.50 | true
 //        }
-//        catch (SQLException | InterruptedException ex) {
+//        catch (SQLException ex) {
 //            ex.printStackTrace();
 //        }
 
